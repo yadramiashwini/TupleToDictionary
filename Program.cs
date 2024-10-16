d75e4b8716589bfe3df2d81b44f3fbd89d9949fb
Ashwini Yadrami <yadramiash192380@gmail.com>
2024-10-16 21:59:50 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace TupleToDictionary
+{
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Create a list of tuples
+            List<Tuple<string, int>> tuples = new List<Tuple<string, int>>();
+            tuples.Add(Tuple.Create("apple", 3));
+            tuples.Add(Tuple.Create("banana", 5));
+            tuples.Add(Tuple.Create("orange", 2));
+
+            // Convert the list of tuples to a dictionary
+            Dictionary<string, int> dictionary = tuples.ToDictionary(tuple => tuple.Item1, tuple => tuple.Item2);
+
+            // Access elements in the dictionary
+            foreach (KeyValuePair<string, int> kvp in dictionary)
+            {
+                Console.WriteLine("Key: " + kvp.Key + ", Value: " + kvp.Value);
+            }
+        }
+    }
+}
